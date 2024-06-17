8aaf0c98056d42a659a31c67d3f81caeabb59852
Mbami Luka <mbamiluka@gmail.com>
2024-06-17 05:48:41 -0400

    remove unused var + frontend Skill UI

--- backend/portfolio/src/main/java/com/mbami/portfolio/model/User.java
+++ backend/portfolio/src/main/java/com/mbami/portfolio/model/User.java
@@ -29,4 +29,60 @@ public class User {
         this.status = status;
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAuthRole() {
+        return authRole;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setAuthRole(String authRole) {
+        this.authRole = authRole;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
