0c9a270f8b12a26f52600fac02017adcf674d739
Mbami Luka <mluka@msudenver.edu>
2025-04-09 06:44:55 -0400

    Add CORS support to HealthController for cross-origin requests

--- backend/portfolio/src/main/java/com/mbami/portfolio/controller/HealthController.java
+++ backend/portfolio/src/main/java/com/mbami/portfolio/controller/HealthController.java
@@ -1,12 +1,14 @@
 package com.mbami.portfolio.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class HealthController {
 
     @GetMapping("/health")
